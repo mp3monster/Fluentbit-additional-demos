658860299d71f4895ab9130c57f6933c3fc34e93
Phil Wilkins <phil@mp3monster.org>
2024-02-21 14:25:28 +0000

    parameterized script

--- flb-slack/get-curl.bat
+++ flb-slack/get-curl.bat
@@ -1,4 +1,4 @@
-curl  -d "channel=CMM13QSBB" -d "limit=3" -d "include_all_metadata=false" -H "Authorization: Bearer xoxb-735037803329-6179690984166-MLSoE7JpBbyIQ2i1MDkFmqQY" -X POST https://slack.com/api/conversations.history
+curl  -d "channel=%slack-channel-id%" -d "limit=3" -d "include_all_metadata=false" -H "Authorization: Bearer %slack-token%" -X POST https://slack.com/api/conversations.history
 REM https://api.slack.com/methods/conversations.history
 REM add oldest - with the timestamp seconds.nanoseconds -- then we can limit 
 REM 'curl  -d "channel=CHANNELID" -d "limit=3" -H "Authorization: Bearer xoxb-YOUR TOKEN HERE" -X POST https://slack.com/api/conversations.history
