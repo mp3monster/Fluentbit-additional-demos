83a557ef4caae752f48de168c0514062a4591468
Phil Wilkins <phil@mp3monster.org>
2024-02-21 14:26:33 +0000

    tailor logging

--- flb-slack/send-curl.bat
+++
@@ -1,7 +0,0 @@
-curl -d "text=Hi I am a bot that can post messages to any public channel." -d "channel=demo" -H "Authorization: Bearer xoxb-735037803329-6179690984166-MLSoE7JpBbyIQ2i1MDkFmqQY" -X POST https://slack.com/api/chat.postMessage
-REM #s secret:BLAH
-REM #verification token:BLAH
-REM #client id:BLAH.6188777992852
-REM #App ID:BLAH
-REM #Bot OToken:xoxb-YOUR TIOKEN HERE
-REM #demo channel Id:BLAH
