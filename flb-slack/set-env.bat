2994286bc321f44ea993f0fb9dc50907da6b626c
Phil Wilkins <phil@mp3monster.org>
2024-02-23 23:51:39 +0000

    script for setting up the environment variables used across the different components

---
+++ flb-slack/set-env.bat
@@ -0,0 +1,14 @@
+set SLACK-TOKEN=xoxb-<your Slack Token>
+set SLACK-CHANNEL-ID=<channel Id - not the channel name>
+set SLACK-CHANNEL-NAME=<actual channel name>
+set SLACK-WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
+set SOCIAL_NODE=<address>:<port>
+set SOCIAL_NODE_ADDR=<address>
+set SOCIAL_NODE_PORT=<port>
+
+SET OPS_RETRYINTERVAL=10
+SET OPS_RETRYCOUNT=12
+SET CHAT-OPS-SVR=<addess>
+SET CHAT-OPS-PORT=<a different port>
+SET TESTFLB=FALSE
+set SYNTH_ALERT_PORT=<another port>
