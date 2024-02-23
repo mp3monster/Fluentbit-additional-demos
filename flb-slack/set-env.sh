d91b259aa3c7b0de9cec8d656b32eca1f2f8705c
Phil Wilkins <phil@mp3monster.org>
2024-02-23 23:54:14 +0000

    Linux script to setup env vars used

---
+++ flb-slack/set-env.sh
@@ -0,0 +1,14 @@
+export SLACK-TOKEN=xoxb-<your Slack Token>
+export SLACK-CHANNEL-ID=<channel Id - not the channel name>
+export SLACK-CHANNEL-NAME=<actual channel name>
+export SLACK-WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
+export SOCIAL_NODE=<address>:<port>
+export SOCIAL_NODE_ADDR=<address>
+export SOCIAL_NODE_PORT=<port>
+
+export OPS_RETRYINTERVAL=10
+export OPS_RETRYCOUNT=12
+export CHAT-OPS-SVR=<addess>
+export CHAT-OPS-PORT=<a different port>
+export TESTFLB=FALSE
+export SYNTH_ALERT_PORT=<another port>
