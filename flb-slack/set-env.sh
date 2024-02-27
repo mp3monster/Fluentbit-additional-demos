8969410b159f81104fc9d1783572c3c6b2c6b192
Phil Wilkins <mp3monster@users.noreply.github.com>
2024-02-27 22:28:08 +0000

    Update set-env.sh
    swap hyphens for underscores

--- flb-slack/set-env.sh
+++ flb-slack/set-env.sh
@@ -1,14 +1,14 @@
-export SLACK-TOKEN=xoxb-<your Slack Token>
-export SLACK-CHANNEL-ID=<channel Id - not the channel name>
-export SLACK-CHANNEL-NAME=<actual channel name>
-export SLACK-WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
+export SLACK_TOKEN=xoxb-<your Slack Token>
+export SLACK_CHANNEL_ID=<channel Id - not the channel name>
+export SLACK_CHANNEL_NAME=<actual channel name>
+export SLACK_WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
 export SOCIAL_NODE=<address>:<port>
 export SOCIAL_NODE_ADDR=<address>
 export SOCIAL_NODE_PORT=<port>
 
 export OPS_RETRYINTERVAL=10
 export OPS_RETRYCOUNT=12
-export CHAT-OPS-SVR=<addess>
-export CHAT-OPS-PORT=<a different port>
+export CHAT_OPS_SVR=<addess>
+export CHAT_OPS_PORT=<a different port>
 export TESTFLB=FALSE
 export SYNTH_ALERT_PORT=<another port>
