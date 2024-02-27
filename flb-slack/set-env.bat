b050e855e0b07c371a988b95ba818958b0305d9a
Phil Wilkins <mp3monster@users.noreply.github.com>
2024-02-27 22:27:11 +0000

    Update set-env.bat
    swap dashes for underscores as hyphens arent liked in Linux envs

--- flb-slack/set-env.bat
+++ flb-slack/set-env.bat
@@ -1,14 +1,14 @@
-set SLACK-TOKEN=xoxb-<your Slack Token>
-set SLACK-CHANNEL-ID=<channel Id - not the channel name>
-set SLACK-CHANNEL-NAME=<actual channel name>
-set SLACK-WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
+set SLACK_TOKEN=xoxb-<your Slack Token>
+set SLACK_CHANNEL_ID=<channel Id - not the channel name>
+set SLACK_CHANNEL_NAME=<actual channel name>
+set SLACK_WEBHOOK=https://hooks.slack.com/services/<your slack webhook>
 set SOCIAL_NODE=<address>:<port>
 set SOCIAL_NODE_ADDR=<address>
 set SOCIAL_NODE_PORT=<port>
 
 SET OPS_RETRYINTERVAL=10
 SET OPS_RETRYCOUNT=12
-SET CHAT-OPS-SVR=<addess>
-SET CHAT-OPS-PORT=<a different port>
+SET CHAT_OPS_SVR=<addess>
+SET CHAT_OPS_PORT=<a different port>
 SET TESTFLB=FALSE
 set SYNTH_ALERT_PORT=<another port>
